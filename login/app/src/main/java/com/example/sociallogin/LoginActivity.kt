eddf8cdd2950e6ac702376095d476aad4df1753b
onxmoreplz <hamdoyoung95@gmail.com>
2021-08-26 21:16:53 +0900

    [Refac] 기존 카카오 로그인 콜백 리스너 수정
    - 기존 로그인 정보 있을 경우,  앱 실행시 바로 메인화면으로 이동
    - 기존 로그인 정보 없을 경우, 앱 실행시 소셜 로그인 화면으로 이동

--- login/app/src/main/java/com/example/sociallogin/LoginActivity.kt
+++ login/app/src/main/java/com/example/sociallogin/LoginActivity.kt
@@ -3,10 +3,10 @@ package com.example.sociallogin
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.user.UserApiClient
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -15,27 +15,40 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        setCallbackFromSocialLogin()
+        setKakaoLoginBtn()
+        setNaverLoginBtn()
+    }
+
+    private val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e("MAIN_SET_CALLBACK", "로그인 실")
+        } else if (token != null) {
+            Log.d("MAIN_SET_CALLBACK", token.toString())
+            startMainActivity()
+        }
     }
 
 
-    private fun setCallbackFromSocialLogin() {
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                Log.e("MAIN_SET_CALLBACK", error.message.toString())
-            } else if (token != null) {
-                Log.d("MAIN_SET_CALLBACK", token.toString())
-                startMainActivity()
-            }
-        }
+    private fun setNaverLoginBtn() {
+        btn_naver_login.setOnClickListener {
 
+        }
+    }
+
+
+    private fun setKakaoLoginBtn() {
         btn_kakao_login.setOnClickListener {
-            Toast.makeText(this, "cpal", Toast.LENGTH_SHORT).show()
-            LoginClient.instance.run {
-                if (isKakaoTalkLoginAvailable(this@LoginActivity)) {
-                    loginWithKakaoTalk(this@LoginActivity, callback = callback) // 카카오톡으로 로그
-                } else {
-                    loginWithKakaoAccount(this@LoginActivity, callback = callback) // 카카오톨 계정으로 로그인(브라우저 기반)
+            UserApiClient.instance.me { user, error ->
+                if (error != null) { // 기존 로그인 정보 없는 경 -> 로그인 시도
+                    LoginClient.instance.run {
+                        if (isKakaoTalkLoginAvailable(this@LoginActivity)) { //카카오톡 앱이 있을 경우
+                            loginWithKakaoTalk(this@LoginActivity, callback = callback) // 카카오톡으로 로그인
+                        } else {
+                            loginWithKakaoAccount(this@LoginActivity, callback = callback) // 카카오톨 계정으로 로그인(브라우저 기반)
+                        }
+                    }
+                } else { //기존 로그인 정보 있을 경우
+                    startMainActivity()
                 }
             }
         }
