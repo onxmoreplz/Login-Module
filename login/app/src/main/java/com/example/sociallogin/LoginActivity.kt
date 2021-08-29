09f1f8084b60ef8e214354814ae27fe85ac3419f
onxmoreplz <hamdoyoung95@gmail.com>
2021-08-29 23:03:44 +0900

    [Add] 네이버 로그인 핸들러 등록

--- login/app/src/main/java/com/example/sociallogin/LoginActivity.kt
+++ login/app/src/main/java/com/example/sociallogin/LoginActivity.kt
@@ -1,24 +1,34 @@
 package com.example.sociallogin
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
+import com.nhn.android.naverlogin.OAuthLogin
+import com.nhn.android.naverlogin.OAuthLoginHandler
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var nhnOAuthLoginModule: OAuthLogin
+    private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        context = this
+
         setKakaoLoginBtn()
         setNaverLoginBtn()
     }
 
+    //카카오 로그인 콜백리스너
     private val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
             Log.e("MAIN_SET_CALLBACK", "로그인 실")
@@ -28,10 +38,33 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-
     private fun setNaverLoginBtn() {
-        btn_naver_login.setOnClickListener {
+        nhnOAuthLoginModule = OAuthLogin.getInstance() //로그인 인스턴스 초기화
+        nhnOAuthLoginModule.init(
+            this,
+            R.string.naver_client_id.toString(),
+            R.string.naver_client_secret.toString(),
+            R.string.naver_client_name.toString()
+        )
+        btn_naver_login.setOAuthLoginHandler(oAuthLoginHandler)
+    }
 
+    // 네이버 로그인 핸들
+    private val oAuthLoginHandler: OAuthLoginHandler = object : OAuthLoginHandler() {
+        override fun run(success: Boolean) {
+            if (success) {
+                val accessToken: String = nhnOAuthLoginModule.getAccessToken(baseContext)
+                val refreshToken: String = nhnOAuthLoginModule.getRefreshToken(baseContext)
+                val expiresAt: Long = nhnOAuthLoginModule.getExpiresAt(baseContext)
+            } else {
+                val errorCode: String = nhnOAuthLoginModule.getLastErrorCode(context).code
+                val errorDesc = nhnOAuthLoginModule.getLastErrorDesc(context)
+
+                Toast.makeText(
+                    baseContext, "errorCode : " + errorCode
+                            + "errorDescription : " + errorDesc, Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
@@ -57,5 +90,14 @@ class LoginActivity : AppCompatActivity() {
     private fun startMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    fun successNaverLogin() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    companion object {
+        private val TAG = LoginActivity::class.java!!.simpleName
     }
 }
