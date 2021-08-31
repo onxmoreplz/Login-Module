57a8fab86b527d8abd308c7068665afbe5352b63
onxmoreplz <hamdoyoung95@gmail.com>
2021-08-31 22:45:47 +0900

    [Add] 메인화면 GoogleAnalytics 사용을 위한 버튼 등록 및 이벤트 추가

--- login/app/src/main/java/com/example/sociallogin/MainActivity.kt
+++ login/app/src/main/java/com/example/sociallogin/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.sociallogin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private var firebaseAnalytics: FirebaseAnalytics? = null
+
+    private val bundle = Bundle()
+    private val buttons = ArrayList<Button>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setButtons()
+        setWebView()
+        setFirebaseAnalytics()
+    }
+
+    private fun setButtons() {
+        buttons.add(btn_1)
+        buttons.add(btn_2)
+        buttons.add(btn_3)
+        btn_1.setOnClickListener { addDataToFirebaseAnalytics(btn_1.toString()) }
+        btn_2.setOnClickListener { addDataToFirebaseAnalytics(btn_1.toString()) }
+        btn_3.setOnClickListener { addDataToFirebaseAnalytics(btn_1.toString()) }
+    }
+
+    private fun setFirebaseAnalytics() {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+    }
+
+    private fun setWebView() {
+        wb_temp.loadUrl("https://www.google.com")
+    }
+
+    private fun addDataToFirebaseAnalytics(btnName: String) {
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_NAME, "버튼 : $btnName")
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_CLASS, "MainActivity")
+
+        this.firebaseAnalytics?.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
